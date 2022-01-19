84e19feffd9f2e39dbd5eda7f93ae708a990f7b9
AdamMousley <AMousley@spartaglobal.com>
2022-01-19 14:08:50 +0000

    A dataframe has been created with the imdb data

--- project1/app/extract_csv.py
+++ project1/app/extract_csv.py
@@ -1,16 +1,17 @@
 import pandas as pd
 import csv
 
-# with open("imdb_data.csv", encoding='utf8') as csv_file:
-#     info = csv.reader(csv_file, delimiter=',')
-#     for row in info:
-#         print(row)
+# def extracting_csv(filename):
+#     with open(filename, encoding='utf8') as csv_file:
+#         info = csv.reader(csv_file, delimiter=',')
+#         for row in info:
+#             print(row)
+#
+# x = "imdb_data.csv"
+# extracting_csv(x)
 
-def extract_csv(filename):
-    with open(filename, encoding='utf8') as csv_file:
-        info = csv.reader(csv_file, delimiter=',')
-        for row in info:
-            print(row)
+def csv_to_dataframe(filename):
+    imdb_df = pd.read_csv(filename)
+    return imdb_df
 
-x = "imdb_data.csv"
-extract_csv(x)
+print(csv_to_dataframe("imdb_data.csv"))
--- project1/test/testing.py
+++ project1/test/testing.py
@@ -1,9 +1,12 @@
 import unittest
+from app.extract_csv import *
 
+class imdb_test(unittest.TestCase):
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    # def test_csv_to_dataframe(self):
+    #     dummy_data = "testingcsv.csv"
+    #     csv_to_dataframe(dummy_data)
+
 
 
 if __name__ == '__main__':
