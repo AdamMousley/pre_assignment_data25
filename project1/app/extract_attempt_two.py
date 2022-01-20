95207438ba9a47f570d9bb2ff255900b920af01a
AdamMousley <AMousley@spartaglobal.com>
2022-01-20 16:02:33 +0000

    both ascii values and and empty values have been removed, need to convert to a csv

--- project1/app/extract_attempt_two.py
+++ project1/app/extract_attempt_two.py
@@ -1,4 +1,6 @@
 import csv
+from unidecode import unidecode
+
 
 def read_csv(filename):
     rows = []
@@ -8,6 +10,7 @@ def read_csv(filename):
         for row in csvreader:
             rows.append(row)
     return rows
+
 
 def select_row_elements(filename):
     missing_data = []
@@ -20,10 +23,31 @@ def select_row_elements(filename):
                 break
     return missing_data,complete_data
 
-def remove_duplicates(data_list):
-    no_duplicates = []
-    for i in data_list:
-        if i not in no_duplicates:
-            no_duplicates.append(i)
-    return no_duplicates
+# def remove_duplicates(data_list):
+#     no_duplicates = []
+#     for i in data_list:
+#         if i not in no_duplicates:
+#             no_duplicates.append(i)
+#     return no_duplicates
+
+def isacii(s):
+    try:
+        s.encode('ascii')
+    except UnicodeEncodeError:
+        return False
+    else:
+        return True
+
+def remove_ascii(filename):
+    contains_ascii = []
+    no_ascii = select_row_elements(filename)[1]
+    for row in select_row_elements(filename)[1]:
+        for i in row:
+            if isacii(i) is False:
+                contains_ascii.append(row)
+                no_ascii.remove(row)
+                break
+    return contains_ascii, no_ascii
+
+
 
--- project1/app/main.py
+++ project1/app/main.py
@@ -9,13 +9,14 @@ rows_w_empty_data = select_row_elements("imdb_data.csv")[0]
 rows_wo_empty_data = select_row_elements("imdb_data.csv")[1]
 
 # Calling the function that removes duplicates in the data without empty values
-wo_empty_remove_dups = remove_duplicates(rows_wo_empty_data)
+# wo_empty_remove_dups = remove_duplicates(rows_wo_empty_data)
+#
+# # Calling the function that removes duplicates in the data with empty values
+# w_empty_remove_dups = remove_duplicates(rows_w_empty_data)
 
-# Calling the function that removes duplicates in the data with empty values
-w_empty_remove_dups = remove_duplicates(rows_w_empty_data)
-
-
-
+# Calls the function to have two different list that contain and don't contain ascii values
+print(remove_ascii('imdb_data.csv')[0])
+print(remove_ascii("imdb_data.csv")[1])
 
 # print(len(rows_w_empty_data))
 # print(len(rows_wo_empty_data))
